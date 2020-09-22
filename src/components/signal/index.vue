80e9a605696e79923ecfbe2bb4239914263ff6c5
caotongxue <952508182@qq.com>
2020-09-22 23:24:14 +0800

    用户模式载入按钮

--- src/components/signal/index.vue
+++ src/components/signal/index.vue
@@ -19,9 +19,15 @@
           <span class="content-list-title">用户模式</span>
         </template>
         <div class="content-list">
-          <div class="list_item" v-for="(item,index) in userSceneList">
-            用户模式 {{index+1}}
-          </div>
+          <template v-for="(item,index) in userSceneList">
+            <div class="list_item" @click="selectedSceneIndex=index">
+              {{item.label}}
+            </div>
+            <div class="list_item" v-show="selectedSceneIndex==index">
+              <el-button size="mini" @click="loadScene">载入</el-button>
+            </div>
+          </template>
+
         </div>
       </el-collapse-item>
       <el-collapse-item name="2">
@@ -51,6 +57,7 @@ export default {
         return {
             inputCardList:[],
             userSceneList:[],//用户模式 列表
+            selectedSceneIndex:-1,//选中的用户模式
 
             presetList:[],
             scenePollingList:[],
@@ -152,6 +159,19 @@ export default {
                     });
                 }
             }
+        },
+        loadScene(){
+            if(this.userSceneList[this.selectedSceneIndex].value==0){
+                alert("该模式没有内容");
+            }
+            else{
+                //载入用户模式
+
+                this.$http.post("loadPreset.cgi",{presetId:this.selectedSceneIndex},(ret)=>{
+                    console.log("signal/index.vue 载入用户模式");
+                    console.log(ret.data);
+                });
+            }
         }
     },
     components:{
@@ -159,7 +179,14 @@ export default {
     },
     watch:{
         "globalEvent.commonInfo":function (v,ov) {
-            this.userSceneList=v.presetStaArr;
+            // this.userSceneList=v.presetStaArr;
+            this.userSceneList=[];
+            for(let i in v.presetStaArr){
+                this.userSceneList.push({
+                    label:'用户模式 '+(this.int(i)+1),
+                    value:v.presetStaArr[i]
+                });
+            }
         }
     }
 
@@ -200,7 +227,8 @@ export default {
     .card_label_valid{position:relative;display:inline-block;}
     .card_label_valid::after{content:' ';width:10px;height:10px;border-radius:10px;position:absolute;background-color:#00cc99;top: 7px;left: -13px;}
     .el-tree-node__content .is-current{background-color:#f3f2f0;}
-    .list_item{cursor:pointer;border: 1px solid #dcdcdc;border-radius: 5px;margin-top: 5px;width: 150px;}
+    .list_item{cursor:pointer;border: 1px solid #dcdcdc;border-radius: 5px;margin-top: 5px;width: 150px;height:30px;line-height:30px;}
+    .list_item:nth-child(even){border:none;}
     .el-collapse-item__content{max-height:500px;overflow:auto;}
   .selected_card .card_label{color:#00cc99;}
 }
